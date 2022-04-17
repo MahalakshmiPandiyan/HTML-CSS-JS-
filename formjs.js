02e2366c7796c2947e0a38b65faf458ffac05300
MahalakshmiPandiyan <96566014+MahalakshmiPandiyan@users.noreply.github.com>
2022-04-17 20:27:03 +0530

    Assignment form

--- formjs.js
+++ formjs.js
@@ -1,279 +1,308 @@
+const steps=Array.from(document.querySelectorAll('form .step'));
+const nextBtn=document.querySelectorAll('form .next-btn');
+const prevBtn=document.querySelectorAll('form .previous-btn');
+
+nextBtn.forEach(button =>{
+    button.addEventListener('click',()=>{
+        changeStep('next');
+    })
+})
+prevBtn.forEach(button =>{
+    button.addEventListener('click',()=>{
+        changeStep('prev');
+    })
+})
+
+
+function changeStep(btn){
+    let index=0;
+    const active=document.querySelector('form .step.active');
+    index=steps.indexOf(active);
+    steps[index].classList.remove('active')
+    if(btn=='next')
+    {
+        if(index==0)
+        {
+            if(firstNameValidate()==true && lastNameValidate()==true && emailIdValidate()==true)
+            {
+                index ++;
+            }
+            else{
+                index=0;
+            }
+            
+        }
+        else if(index==1)
+        {
+            if(dateValidate()===true && stateValidate()==true && phoneNumberValidate()==true )
+            {
+                index++;
+            }
+            else{
+                index=1;
+            }
+        }
+        else if(index==2)
+        {
+          index++;
+        }
+        else if(index==3)
+        {
+          index++;
+        }
+        else if(index==4)
+        {
+          index++;
+        }
+
+    }
+    else if(btn='prev')
+    {
+        index --;
+    }
+    steps[index].classList.add('active')
+    console.log(index);
+    console.log(active);
+
+}
 
 function firstNameValidate() {
-  var idfirst = document.getElementById("id1");
-  if (firstName.value.trim() === "") {
-    onError(firstName, "First name cannot be empty", idfirst);
+    var idfirst = document.getElementById("id1");
+    if (firstName.value.trim() === "") {
+      onError(firstName, "First name cannot be empty", idfirst);
+    }
+    else if (!nameValidation(firstName.value.trim())) {
+      onError(firstName, "First name should contain only letters", idfirst);
+    }
+    else {
+      onSuccess(firstName, idfirst);
+      return true;
+  
+    }
   }
-  else if (!nameValidation(firstName.value.trim())) {
-    onError(firstName, "First name should contain only letters", idfirst);
+  function lastNameValidate() {
+    var idlast = document.getElementById("id2");
+    if (lastName.value.trim() === "") {
+      onError(lastName, "Last name cannot be empty", idlast);
+    }
+    else if (!nameValidation(lastName.value.trim())) {
+      onError(lastName, "Last name should contain only letters", idlast);
+    }
+    else {
+      onSuccess(lastName, idlast);
+      return true;
+  
+    }
   }
-  else {
-    onSuccess(firstName, idfirst);
-    return true;
 
+  function emailIdValidate() {
+    var idemail = document.getElementById("id3");
+  
+    if (emailId.value.trim() === "") {
+      onError(emailId, "Email Id cannot be empty", idemail);
+    }
+    else if (!emailIdValidation(emailId.value.trim())) {
+      onError(emailId, "Invalid Email Id", idemail);
+    }
+    else {
+      onSuccess(emailId, idemail);
+      return true;
+  
+    }
+  }
+
+  function phoneNumberValidate() {
+    var idphn = document.getElementById("id10");
+    if (phoneNo.value.trim() === "") {
+      onError(phoneNo, "Phone Number cannot be empty", idphn);
+    }
+    else if (!phoneNumberValidation(phoneNo.value.trim())) {
+      onError(phoneNo, "Phone Number should start with [6-9] followed by 9 digits", idphn);
+    }
+    else {
+      onSuccess(phoneNo, idphn);
+      return true;
+  
+    }
+  }
+  
+  
+  function dateValidate() {
+    var idDate = document.getElementById("id6");
+  
+    if (setDate.value.trim() === "") {
+      onError(setDate, "Date name not selected", idDate);
+    }
+    else if (dateValidation(setDate.value.trim())) {
+      onError(setDate, "Select Today's Date", idDate);
+    }
+    else {
+      onSuccess(setDate, idDate);
+      return true;
+  
+    }
+  }
+
+
+  function stateValidate()
+  {
+    var idState=document.getElementById("id9");
+    if (!stateValidation(state.value.trim())) {
+      onError(state, "State not selected", idState);
+    }
+    else {
+      onSuccess(state, idState);
+      return true;
+    }
+  }
+
+  function departmentValidate()
+  {
+    var idDep=document.getElementById("depSmall")
+    if (!departmentValidation(department.value.trim())) {
+      onError(department, "Department not selected", idDep);
+    }
+    else {
+      onSuccess(department, idDep);
+      return true;
+    }
+  }
+
+  function checkValidate()
+  {
+    var idCheck=document.getElementById("checkSmall");
+    if (!checkValidation(check.value.trim())) {
+      onError(check, "check not selected", idCheck);
+    }
+    else {
+      onSuccess(check, idCheck);
+      return true;
   }
 }
-function lastNameValidate() {
-  var idlast = document.getElementById("id2");
-  if (lastName.value.trim() === "") {
-    onError(lastName, "Last name cannot be empty", idlast);
-  }
-  else if (!nameValidation(lastName.value.trim())) {
-    onError(lastName, "Last name should contain only letters", idlast);
-  }
-  else {
-    onSuccess(lastName, idlast);
-    return true;
-
-  }
-}
-
-function emailIdValidate() {
-  var idemail = document.getElementById("id3");
-
-  if (emailId.value.trim() === "") {
-    onError(emailId, "Email Id cannot be empty", idemail);
-  }
-  else if (!emailIdValidation(emailId.value.trim())) {
-    onError(emailId, "Invalid Email Id", idemail);
-  }
-  else {
-    onSuccess(emailId, idemail);
-    return true;
-
-  }
-}
-
-function phoneNumberValidate() {
-  var idphn = document.getElementById("id4");
-  if (phoneNo.value.trim() === "") {
-    onError(phoneNo, "Phone Number cannot be empty", idphn);
-  }
-  else if (!phoneNumberValidation(phoneNo.value.trim())) {
-    onError(phoneNo, "Phone Number should start with [6-9] followed by 9 digits", idphn);
-  }
-  else {
-    onSuccess(phoneNo, idphn);
-    return true;
-
-  }
-}
-
-
-function domainValidate() {
-  var idDomain = document.getElementById("id5");
-
-  if (!domainNameValidation(domainName.value.trim())) {
-    onError(domainName, "Domain name not selected", idDomain);
-  }
-  else {
-    onSuccess(domainName, idDomain);
-    return true;
-
-  }
-}
-function dateValidate() {
-  var idDate = document.getElementById("id6");
-
-  if (setDate.value.trim() === "") {
-    onError(setDate, "Date name not selected", idDate);
-  }
-  else if (dateValidation(setDate.value.trim())) {
-    onError(setDate, "Select Today's Date", idDate);
-  }
-  else {
-    onSuccess(setDate, idDate);
-    return true;
-
-  }
-}
-
-function aadharValidate() {
-  var idAadhar = document.getElementById("id7");
-  if (aadharValue.value.trim() === "") {
-    onError(aadharValue, "Aadhar number cannot be empty", idAadhar);
-  }
-  else if (aadharValidation(aadharValue.value.trim())) {
-    onError(aadharValue, "Invalid Aadhar number", idAadhar);
-  }
-  else {
-    onSuccess(aadharValue, idAadhar);
-    return true;
-
-  }
-
-}
-function panValidate() {
-
-  var idPan = document.getElementById("id8");
-  if (panValue.value.trim() === "") {
-    onError(panValue, "Aadhar number cannot be empty", idPan);
-  }
-  else if (panValidation(panValue.value.trim())) {
-    onError(panValue, "Invalid Aadhar number", idPan);
-  }
-  else {
-    onSuccess(panValue, idPan);
-    return true;
-  }
-
-}
-
 
 
 let firstName = document.getElementById("firstname");
 let lastName = document.getElementById("lastname");
-let companyName = document.getElementById("company");
 let emailId = document.getElementById("email");
-let phoneNo = document.getElementById("phoneNumber");
-let domainName = document.getElementById("domain");
 let setDate = document.getElementById("date");
-let aadharValue = document.getElementById("aadhar");
-let panValue = document.getElementById("pan");
+let state = document.getElementById("state");
+let phoneNo = document.getElementById("phoneNumber");
+let department = document.getElementById("dep");
+let check=document.getElementById("check");
+
 
 firstName.addEventListener('blur', firstNameValidate);
 lastName.addEventListener('blur', lastNameValidate);
 emailId.addEventListener('blur', emailIdValidate)
-phoneNo.addEventListener('blur', phoneNumberValidate);
-domainName.addEventListener('blur', domainValidate);
 setDate.addEventListener('blur', dateValidate);
-aadharValue.addEventListener('blur', aadharValidate);
-panValue.addEventListener('blur', panValidate);
+state.addEventListener('blur', stateValidate);
+phoneNo.addEventListener('blur', phoneNumberValidate);
+department.addEventListener('blur', departmentValidate);
+check.addEventListener('blur', checkValidate);
 
 
 function onSuccess(input, id) {
-  id.innerHTML = "";
-  input.classList.add('success')
-}
-
-function onError(input, message, id) {
-  id.innerHTML = message;
-  input.classList.add('error');
-}
-
-function nameValidation(name) {
-  var firstNameRegx = /^[a-zA-Z]{3,15}$/;
-  if (name.match(firstNameRegx)) {
-    return true;
+    id.innerHTML = "";
   }
-  else {
-    return false;
+  
+  function onError(input, message, id) {
+    id.innerHTML = message;
+    input.classList.add('error');
   }
-}
-
-function emailIdValidation(emailId) {
-  var emailIdRegx = /^[a-z0-9]{5,13}@[a-z]+\.[a-z\.]{2,6}$/;
-  if (emailId.match(emailIdRegx)) {
-    return true;
-  }
-  else {
-    return false;
-  }
-
-}
-function phoneNumberValidation(phoneNo) {
-  var phoneNumberRegx = /^[6-9]{1}[0-9]{9}$/;
-  if (phoneNo.match(phoneNumberRegx)) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-function domainNameValidation(domainName) {
-  if (domainName === "") {
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-function dateValidation(setDate) {
-  const otherDate = new Date(setDate);
-  const todayDate = new Date();
-  if (otherDate.getDate() === todayDate.getDate() &&
-    otherDate.getMonth() === todayDate.getMonth() &&
-    otherDate.getYear() === todayDate.getYear()) {
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-function aadharValidation(aadharValue) {
-  var aadharRgex = /^[2-9]{1}[0-9]{3}\s{1}[0-9]{4}\s{1}[0-9]{4}$/;
-  if (aadharValue.match(aadharRgex)) {
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-function panValidation(panValue) {
-  var panRegex = /^([a-zA-Z]){5}([0-9]){4}([a-zA-Z]){1}?$/
-  if (panValue.match(panRegex)) {
-    return false;
-  }
-  else {
-    return true;
-  }
-}
-
-
-
-
-function validateForm() {
-
-  if (firstName.value.trim() === "") {
-    document.getElementById("id1").innerHTML = "This field is neccesary";
-    if (lastName.value.trim() === "") {
-      document.getElementById("id2").innerHTML = "This field is neccesary";
-      if (emailId.value.trim() === "") {
-        document.getElementById("id3").innerHTML = "This field is neccesary";
-        if (phoneNo.value.trim() === "") {
-          document.getElementById("id4").innerHTML = "This field is neccesary";
-          if (domainName.value.trim() === "") {
-            document.getElementById("id5").innerHTML = "This field is neccesary";
-            if (setDate.value.trim() === "") {
-              document.getElementById("id6").innerHTML = "This field is neccesary";
-              if (aadharValue.value.trim() === "") {
-                document.getElementById("id7").innerHTML = "This field is neccesary";
-                if (panValue.value.trim() === "") {
-                  document.getElementById("id8").innerHTML = "This field is neccesary";
-
-                }
-              }
-            }
-          }
-        }
-      }
+  
+  function nameValidation(name) {
+    var firstNameRegx = /^[a-zA-Z]{3,15}$/;
+    if (name.match(firstNameRegx)) {
+      return true;
     }
-  }  
-  else
-  {
-    if(firstNameValidate()==true && lastNameValidate()==true && emailIdValidate()==true && phoneNumberValidate()==true 
-    && domainValidate()==true && dateValidate()== true && (aadharValidate()==true || panValidate()==true)){
+    else {
+      return false;
+    }
+  }
+
+  function emailIdValidation(emailId) {
+    var emailIdRegx = /^[a-z0-9]{5,13}@[a-z]+\.[a-z\.]{2,6}$/;
+    if (emailId.match(emailIdRegx)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  
+  }
+
+  function dateValidation(setDate) {
+    const otherDate = new Date(setDate);
+    const todayDate = new Date();
+    if (otherDate.getDate() === todayDate.getDate() &&
+      otherDate.getMonth() === todayDate.getMonth() &&
+      otherDate.getYear() === todayDate.getYear()) {
+      return false;
+    }
+    else {
       return true;
     }
   }
-  return false;
 
+  function phoneNumberValidation(phoneNo) {
+    var phoneNumberRegx = /^[6-9]{1}[0-9]{9}$/;
+    if (phoneNo.match(phoneNumberRegx)) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  function stateValidation(state)
+  {
+    if (state === "") {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  function departmentValidation(department)
+  {
+    if (department === "") {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+function checkValidation(check)
+{
+  if(check.checked===false)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+  function validateForm() 
+  {
+    if (department.value.trim() === "") 
+    {
+      document.getElementById("depSmall").innerHTML = "This field is neccesary";
+      if(check.checked==false)
+      {
+        document.getElementById("checkSmall").innerHTML = "This field is neccesary";
+      }
+    }  
+    else
+    {
+      if(departmentValidate()== true && check.checked==true)
+      {
+        return true;
+      }
+    }
+    return false;
   
-}
-
-
-function yesnoCheck(arg) {
-  if (arg.value == "aadhar") {
-    document.getElementById("adc").style.display = "block";
+    
   }
-  else {
-    document.getElementById("adc").style.display = "none";
-  }
-  if (arg.value == "pan") {
-    document.getElementById("pc").style.display = "block";
-  }
-  else {
-    document.getElementById("pc").style.display = "none";
-  }
-}
